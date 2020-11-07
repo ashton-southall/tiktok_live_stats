4864defbe235108037696b81a3924717b33fe64e
Ashton Southall <38774365+Adsnipers@users.noreply.github.com>
2020-11-07 13:26:21 +1000

    add video stat tracking

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
 const tiktok = require('tiktok-app-api');
 const chalk = require('chalk');
- 
+
 let tiktokApp;
- 
-function getUserInfo() {
-    (async () => {
-        tiktokApp = await tiktok();
-       
-        const user = await tiktokApp.getUserByName('Adsnipers'); // Put your username here
-        const userInfo = await tiktokApp.getUserInfo(user);
-        console.log(chalk.green(`Followers: ${userInfo.followerCount}`)) // Displays user follower count in green
-        console.log(chalk.cyan(`Likes: ${userInfo.likeCount}`)) // Displays user follower count in cyan
-        
-      })().catch(error => console.error);
-      setTimeout(getUserInfo, 300000) // Wait 5 minutes between requests, this is to help prevent rate limiting
+async function waitForTiktok() {
+  tiktokApp = await tiktok();
 }
-getUserInfo()
+
+console.log(chalk.bgRed(`Please make sure only one tracking task is uncommented, running two will cause serious rate limits and could make the script unusable`))
+
+async function getUserInfo() {
+  console.clear()
+  console.log(chalk.inverse(`TikTok Live Updating Stats by @Adsnipers`))
+  const user = await tiktokApp.getUserByName('Adsnipers'); // Put your username here
+  const userInfo = await tiktokApp.getUserInfo(user);
+  console.log(chalk.bggreen(`Followers: ${userInfo.followerCount}`)) // Displays user follower count in green
+  console.log(chalk.bgGreen(`Likes: ${userInfo.likeCount}`)) // Displays user follower count in cyan
+}
+
+async function getVideoInfo() {
+  console.clear()
+  console.log(chalk.inverse(`TikTok Live Updating Stats by @Adsnipers`))
+  const video = tiktokApp.getVideo('6891996320807128321');
+  const videoInfo = await tiktokApp.getVideoInfo(video);
+  console.log(chalk.bgCyan(videoInfo.description))
+  console.log(chalk.bgCyan(`Views: ${videoInfo.playCount}`))
+  console.log(chalk.bgCyan(`Likes: ${videoInfo.likeCount}`))
+  console.log(chalk.bgCyan(`Comments: ${videoInfo.commentCount}`))
+  console.log(chalk.bgCyan(`Shares: ${videoInfo.shareCount}`))
+}
+
+function run() {
+  waitForTiktok().catch(error => console.log(chalk.bgRed(error)))
+  //getUserInfo().catch(error => console.log(chalk.bgRed(`Error while getting info, could be rate limited`)))
+  //getVideoInfo().catch(error => console.log(chalk.bgRed(error + ' | Probably being rate limited')))
+  setTimeout(run, 50000)
+}
+run()
